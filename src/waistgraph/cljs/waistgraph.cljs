fd5c6a492f1b3f6ac1dda975989cc2b2df218133
rhythmthumper <github3200@jaredbowie.com>
2014-05-24 23:27:09 +0900

    minor experiments

--- src/waistgraph/cljs/waistgraph.cljs
+++ src/waistgraph/cljs/waistgraph.cljs
@@ -4,9 +4,33 @@
 
 (.write js/document "Hello, ClojureScript!")
 
+(def data
+  {:labels ["January","February","March","April","May","June","July"]
+   :datasets [{:fillColor "rgba(220,220,220,0.5)"
+               :strokeColor "rgba(220,220,220,1)",
+               :pointColor "rgba(220,220,220,1)",
+               :pointStrokeColor "#fff",
+               :data [65,59,90,81,56,55,40]
+               }
+              	{
+                 :fillColor "rgba(151,187,205,0.5)",
+                 :strokeColor "rgba(151,187,205,1)",
+                 :pointColor "rgba(151,187,205,1)",
+                 :pointStrokeColor "#fff",
+                 :data [28,48,40,19,96,27,100]
+		}
+              ]
+   }
+  )
+
+
 (defn make-test-graph []
-  (let [ctx (by-id "myChart")]
-    (. js/console (log ctx))
+  (let [canvas-id (by-id "myChart")
+        context-2d (.getContext canvas-id "2d")
+        new-chart (.Line (.new Chart context-2d) data)
+        ]
+    (. js/console (log "hi"))
+    (. js/console (log new-chart))
     )
   )
 
